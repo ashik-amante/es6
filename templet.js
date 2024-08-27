e52e7457dd8ea3ff888c6abda3ffe57f428cc8e5
Abdullah Al Ashik <abdullahashik17@gmail.com>
2024-08-28 02:02:15 +0600

    backtic as templet

---
+++ templet.js
@@ -0,0 +1,19 @@
+// use of backtic
+
+const a  = 10;
+const b = 12;
+console.log(`the sum of ${a} and ${b} is ${a+b}`);
+
+const message = ` hello
+bal falo`;
+console.log(message);
+
+const number = [12,23,34,45,56]
+const student = {
+    name: 'ashik',
+    age: 10
+
+}
+
+const output = `the message is ${student.name}'s age is ${number[1]} `
+console.log(output);
