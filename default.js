0f4a5611f7d9b0c518b0508c09e3d25a2b62b956
Abdullah Al Ashik <abdullahashik17@gmail.com>
2024-08-28 01:04:39 +0600

    default up

--- default.js
+++ default.js
@@ -1,3 +1,5 @@
+// can assign any value as default
+
 function add(num1,num2=0){
     const sum = num1 + num2;
     console.log(num1, num2, sum);
@@ -6,3 +8,14 @@ function add(num1,num2=0){
 
 const sum = add(2,);
 console.log(sum);
+
+
+function fullName(firstname, lastname = 'uddin'){
+    const name = firstname + ' ' + lastname;
+    return name
+}
+
+const name = fullName('ashik');
+console.log(name);
+const dak = fullName('kamal')
+console.log(dak);
